1965c909a3f9db7a497a9f2feb440f432584ebe6
davisschenk <35881002+davisschenk@users.noreply.github.com>
2019-03-13 22:43:51 -0600

    Added function to check if a user has auth

--- core/general_functions.py
+++ core/general_functions.py
@@ -9,3 +9,9 @@ def load_config():
 def update_config(new_config):
     with open('core/configs.json', 'w') as w:
         json.dump(new_config, w)
+
+
+def check_auth(ctx):
+    if ctx.message.author.id in load_config():
+        return True
+    return False
