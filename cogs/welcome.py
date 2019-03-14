ff09ec9006675b609b124632799fa92e3cd8da93
davisschenk <35881002+davisschenk@users.noreply.github.com>
2019-03-13 23:48:17 -0600

    Multi-Server Support
    Needs cleaned up

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,22 +7,24 @@ class WelcomeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.configs = load_config()
-        self.welcome_channel = self.bot.get_channel(self.configs["welcome_id"])
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         embed = discord.Embed(title="Welcome!", description=f'{member.name} has Joined the Server!', color=583680)
-        await self.welcome_channel.send(embed=embed)
+        channel = self.bot.get_channel(int(self.configs["guilds"][str(member.guild.id)]["welcome_id"]))
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         embed = discord.Embed(title="Goodbye", description=f'{member.name} has Left the Server!', color=16713287)
-        await self.welcome_channel.send(embed=embed)
+        channel = self.bot.get_channel(int(self.configs["guilds"][str(member.guild.id)]["welcome_id"]))
+        await channel.send(embed=embed)
 
     @commands.has_permissions(administrator=True)
     @commands.command(name="set_welcome")
     async def set_welcome_channel(self, ctx, channel: commands.TextChannelConverter):
-        self.configs["welcome_id"] = channel.id
+        """Given a channel it updates the welcome for that guild"""
+        self.configs["guilds"][str(channel.guild.id)] = {"welcome_id": str(channel.id)}
         update_config(self.configs)
         await ctx.send(f'Welcome channel updated to: {channel.name}')
 
