8794b2bb42493bd673f9f8abb3f47d555b92966c
davisschenk <35881002+davisschenk@users.noreply.github.com>
2019-03-14 01:47:52 -0600

    Added steam command

--- cogs/general.py
+++ cogs/general.py
@@ -13,6 +13,14 @@ class GeneralCog(commands.Cog):
                               url='https://github.com/Solo-Hawk/CS-Army-Discord-Reboot')
         await ctx.send(embed=embed)
 
+    @commands.command(name="steam")
+    async def get_steam(self, ctx):
+        embed = discord.Embed(title='CS Army Steam Group',
+                              description='Join the Steam Group!',
+                              url="https://steamcommunity.com/groups/CSArmyNerds")
+        embed.set_image(url="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ae5df07e7d149cd6bd64be8ae8b305ed9dc97d4_full.jpg")
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(GeneralCog(bot))
