8c7c4dbfb78dcb9b23b4852bfa8727aa4db13d3a
davisschenk <35881002+davisschenk@users.noreply.github.com>
2019-06-06 16:12:25 -0600

    Handles missing permissions

--- cogs/ErrorHandling.py
+++ cogs/ErrorHandling.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import traceback
 import sys
 
@@ -29,6 +30,17 @@ class CommandErrorHandler(commands.Cog):
 
         elif isinstance(error, commands.DisabledCommand):
             return await ctx.send("That command is currently disabled")
+
+        elif isinstance(error, commands.CommandInvokeError):
+            original = error.original
+            if isinstance(original, discord.Forbidden):
+                await ctx.send("The Bot is missing permissions")
+
+            else:
+                await ctx.send("Internal Error: " + type(original))
+
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send("You are missing permissions for that command")
 
         print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
